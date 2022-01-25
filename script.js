2b2ba1c2939029294309901b744d84d62cf8b781
GhislainHwn <clovisleron@gmail.com>
2022-01-25 11:25:16 -0300

    updat project

---
+++ script.js
@@ -0,0 +1,21 @@
+for(let i =1; i<= 10; i++) {
+    console.log(i);
+}
+
+let n=10;
+while (n!==0) {console.log(n)
+    n-=1;
+}
+
+i=1
+while (i<=100){
+console.log(i)
+i+=2
+}
+
+i=2
+while (i<=100){
+    console.log(i)
+    i+=2
+    }
+
